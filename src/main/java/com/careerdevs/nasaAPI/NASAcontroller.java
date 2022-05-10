15c9e91bbccac254b8ccbaccd5d497b166aeb519
darielhernandez <13dariel@gmail.com>
2022-05-10 17:21:57 -0400

    Initial commit

---
+++ src/main/java/com/careerdevs/nasaAPI/NASAcontroller.java
@@ -0,0 +1,10 @@
+package com.careerdevs.nasaAPI;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class NASAcontroller {
+
+}
